9e7310c412041f7eefce3b32f7261de4523b772a
sahil pandey <103140300+loopg@users.noreply.github.com>
2024-04-28 00:17:15 +0530

    Update streamlitAPP.py

--- streamlitAPP.py
+++ streamlitAPP.py
@@ -2,7 +2,6 @@ import os
 import json
 import pandas as pd
 import traceback
-from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file,get_table_data
 from src.mcqgenerator.logger import logging
 from src.mcqgenerator.MCQgenerator import generate_evaluate_chain
